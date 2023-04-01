f0681b79e4ae481c7e6be4443109a21f6c7c5392
BART! <bart@bartweb.cz>
2023-04-01 22:46:52 +0200

    feat(button): add missing csfd label

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,6 +1,7 @@
 import { CSFDMovie } from 'node-csfd-api/types/interfaces/movie.interface';
 import type { Component } from 'solid-js';
 import './Button.scss';
+const _ = chrome.i18n.getMessage;
 
 const Button: Component<CSFDMovie> = (movie) => {
   const urlUnknown = `https://www.csfd.cz/?q=${movie.title} ${movie.year}`;
@@ -11,7 +12,7 @@ const Button: Component<CSFDMovie> = (movie) => {
       title={movie.title}
       class={'btn btn-success rating rating-' + movie.colorRating}
     >
-      {movie.rating ?? '?'} %
+      {_('csfd')} {movie.rating ?? '?'} %
     </a>
   );
 };
